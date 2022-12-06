219e50fd664ecf5d785d4b17ec319448d71c6c43
Andrew <90209255+AndzejusN@users.noreply.github.com>
2022-12-06 22:18:01 +0200

    Courses ended / correct concerning Diploma

--- docs/js/app.js
+++ docs/js/app.js
@@ -151,7 +151,7 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkfirst"] = self["webpackChunkfirst"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunkcv"] = self["webpackChunkcv"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
